465cbab2c297c04b7f6cdc490ce346b3ca308171
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2023-10-30 21:00:13 -0300

    Exercicio do Dia 30/10

---
+++ SorteadorJogosLoteria/SegundaSemana/Program.cs
@@ -0,0 +1,22 @@
+﻿using SegundaSemana;
+
+Console.Write("Insira seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Informe quantos reais você tem para jogar: R$ ");
+double totalDeReais = double.Parse(Console.ReadLine()!);
+
+int valorDoJogo = 5;
+
+int quantidadeDeJogos = (int)totalDeReais / valorDoJogo;
+
+Console.WriteLine($"Olá, {nome}. Com seus R$ {totalDeReais.ToString("C2")} é possível realizar {quantidadeDeJogos} jogos.");
+Console.WriteLine();
+Console.WriteLine($"Aqui estão {quantidadeDeJogos} sugestões de jogos:");
+
+SorteadorDeJogo s = new SorteadorDeJogo();
+
+for (int i = 0; i < quantidadeDeJogos; i++)
+{
+    Console.WriteLine($"Jogo {i + 1}: {s.SortearJogo()}");
+}
---
+++ SorteadorJogosLoteria/SegundaSemana/SorteadorDeJogo.cs
@@ -0,0 +1,51 @@
+﻿namespace SegundaSemana
+{
+    class SorteadorDeJogo
+    {
+        public int[] NumerosLoteria = new int[60];
+
+
+        public SorteadorDeJogo()
+        {
+            PreencherLista();
+        }
+
+        private void PreencherLista()
+        {
+            for (int i = 0; i < NumerosLoteria.Length; i++)
+            {
+                NumerosLoteria[i] = i + 1;
+            }
+        }
+
+        public string SortearJogo()
+        {
+            Random random = new Random();
+            int[] jogo = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                while (true)
+                {
+                    int numeroEscolhido = NumerosLoteria[random.Next(0, NumerosLoteria.Length)];
+
+                    if (jogo.Length > 0)
+                    {
+                        if (!jogo.Contains(numeroEscolhido))
+                        {
+                            jogo[i] = numeroEscolhido;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        jogo[i] = numeroEscolhido;
+                    }
+                }
+                
+            }
+            Array.Sort(jogo);
+            return string.Join(", ", jogo);
+        }
+    }
+}
